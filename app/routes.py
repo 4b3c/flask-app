9629085103f42581ced14cdc4e8f9ef8bcf81033
Abram <58718191+4b3c@users.noreply.github.com>
2025-11-14 19:44:35 -0700

    more examples

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,7 @@ def home():
 @main.route("/example")
 def example():
     return render_template("example.html")
+
+@main.route("/example2")
+def example2():
+    return render_template("example2.html")
